72e706e020544b08ebc56397c77bd9a09af4af3b
Mihelle Torres <lic.nancy.torres@gmail.com>
2019-02-22 22:35:07 -0500

    Create components

--- js/main.js
+++ js/main.js
@@ -1,39 +1,84 @@
 // var product = 'Socks';
-var app = new Vue({
-  el: '#app', // The id to use on the html
-  data: {
-    brand: 'Michelle',
-    product: 'Socks',
-    description: 'A pair of warm, fuzzy socks',
-    selectedVariant: 0,
-    inventory: 100,
-    onSale: true,
-    link: '/more-products.html',
-    details: [
-      '80% cotton',
-      '20% polyester',
-      'Gender-natural',
-    ],
-    variants: [
-      {
-        id: 2234,
-        color: 'green',
-        image: './assets/socks.jpeg',
-        quantity: 10,
-      },
-      {
-        id: 2235,
-        color: 'blue',
-        image: './assets/socks-blue.jpeg',
-        quantity: 0,
-      },
-    ],
-    sizes: [
-      'small',
-      'medium',
-      'large',
-    ],
-    cart: 0,
+Vue.component('product', {
+  template: `
+  <div class="product">
+  <div class="product-image">
+  <img :src="image" :alt="product">
+  </div>
+
+  <div class="product-info">
+  <h1>{{ title }}</h1>
+  <p v-show="onSale">On Sale!</p>
+  <p v-if="inStock">In Stock</p>
+  <p v-else>Out of Stock</p>
+  <p>{{ description }}</p>
+  <ul>
+  <li v-for="detail in details">{{ detail }}</li>
+  </ul>
+
+  <p>Variants</p>
+  <div v-for="(variant, index) in variants"
+  :key="variant.id"
+  class="color-box"
+  :style="{ backgroundColor: variant.color }"
+  @mouseover="updateProduct(index)"
+  :aria-label="variant.color" >
+  </div>
+
+  <p>Sizes</p>
+  <ul>
+  <li v-for="size in sizes">{{ size }}</li>
+  </ul>
+  <a :href="link">More products</a>
+  </div>
+
+  <button @click="addToCart"
+  :disabled="!inStock"
+  :class="{ disabledButton: !inStock}" >
+  Add to cart
+  </button>
+  <button @click="removeFromCart" v-show="cart">Remove from cart</button>
+
+  <div class="cart">
+  <p>Cart({{cart}})</p>
+  </div>
+  </div>
+  `,
+  data() {
+    return {
+      brand: 'Michelle',
+      product: 'Socks',
+      description: 'A pair of warm, fuzzy socks',
+      selectedVariant: 0,
+      inventory: 100,
+      onSale: true,
+      link: '/more-products.html',
+      details: [
+        '80% cotton',
+        '20% polyester',
+        'Gender-natural',
+      ],
+      variants: [
+        {
+          id: 2234,
+          color: 'green',
+          image: './assets/socks.jpeg',
+          quantity: 10,
+        },
+        {
+          id: 2235,
+          color: 'blue',
+          image: './assets/socks-blue.jpeg',
+          quantity: 0,
+        },
+      ],
+      sizes: [
+        'small',
+        'medium',
+        'large',
+      ],
+      cart: 0,
+    };
   },
   methods: {
     addToCart() {
@@ -57,4 +102,8 @@ var app = new Vue({
       return this.variants[this.selectedVariant].quantity
     }
   }
+})
+
+var app = new Vue({
+  el: '#app', // The id to use on the html
 })
